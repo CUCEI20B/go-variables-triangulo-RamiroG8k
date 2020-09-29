e23d9906e6b924159a7417b586762ce22a27b751
Ramiro Mendez <ramiro.g8k@gmail.com>
2020-09-29 11:45:47 -0500

    update

--- main.go
+++ main.go
@@ -3,5 +3,8 @@ package main
 import "fmt"
 
 func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+	var base, altura float64
+	fmt.Scan(&base)
+	fmt.Scan(&altura)
+	fmt.Println(base*(altura/2))
 }
